7439348dd856d3e58fa34f11d670f32ea898f81c
Shama <shamaanjum09@outlook.com>
2017-12-03 19:21:08 -0800

    added logic for checking  correct answer and displaying results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,21 +15,37 @@ $(document).ready(function(){
     var inCorrect=0;
     var unanswered=0;
     var container = $("#container");
+
     $("#start").on("click", function(){
         
-        var triviadiv = $("<div id='triviaContainer'>")
-        triviadiv.append("<h2>Time Remaining:<span id='timeRemaining'></span></h2>");
-        triviadiv.append("<ol><li><p>What is the permanent memory built into your computer called?</p><input type='radio' name='q1Choices' id='opt1'>CPU<input type='radio' name='q1Choices' id='opt2'>RAM<input type='radio' name='q1Choices' id='opt3'>ROM<input type='radio' name='q1Choices' id='opt4'>CD-ROM</li></ol>");
-        triviadiv.append("<button id='done' type='submit'>DONE</button>");
+        var triviadiv = $("<div id='triviaContainer'>");
+        var timeRemaining= "<h2>Time Remaining:<span id='timeRemaining'></span></h2>";
+        var questions= "<ol><li><p>What is the permanent memory built into your computer called?</p>"+
+        "<input type='radio' name='q1Choices' id='opt1' value='CPU'>CPU"+
+        "<input type='radio' name='q1Choices' id='opt2' value='RAM'>RAM"+
+        "<input type='radio' name='q1Choices' id='opt3' value='ROM'>ROM"+
+        "<input type='radio' name='q1Choices' id='opt4' value='CD-ROM'>CD-ROM</li></ol>"+
+        "<input type='hidden' class='answer' value='ROM'>";
+        var done= "<button id='done' type='submit'>DONE</button>";
+        triviadiv.append(timeRemaining);
+        triviadiv.append(questions);
+        triviadiv.append(done);
         container.html(triviadiv);
         $("#timeRemaining").text(time+" seconds");
         intervalId=setInterval(runTimer,1000);
-
-
+        console.log("start completed!!!");
+        
     });
-    $("#done").on("click", function(){
+  
+    // when user clicks done button display results
+    
+    $("#container").on("click", "#done", function(){
+        console.log("start completed!!!");
+        console.log("done");
+        clearInterval(intervalId);
         displayResults();
     });
+
     // function to run timer and display results
     function runTimer(){
         time--;
@@ -41,6 +57,22 @@ $(document).ready(function(){
     }
     // function to display results once game is over
     function displayResults(){
+        // get checked value and correct value and compare both to display appropriate result
+        var selectedVal = "";
+        var selected = $("input[type='radio'][name='q1Choices']:checked");
+        var correctAnswer= $(".answer").val();
+        if (selected.length > 0) {
+            selectedVal = selected.val();
+            if(selectedVal===correctAnswer){
+                correct++;
+            }
+            else {
+                inCorrect++;
+            }
+        }
+        else{
+            unanswered++;
+        }
         container.html("<div id='results'>");
         container.append("<h3>All Done!</h3>");
         container.append("<p> Correct Answers:"+correct+"</p>");
