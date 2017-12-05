5ccba579d6832185996005185dc8bb7246789fcd
Shama <shamaanjum09@outlook.com>
2017-12-04 16:25:14 -0800

    added new questions, css styling and bg images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     // set timer variable
     // set variable to hold intervalId
     // counter for answers
-    var time = 10;
+    var time = 30;
     var intervalId;
     var correct = 0;
     var inCorrect = 0;
@@ -25,33 +25,116 @@ $(document).ready(function () {
         ],
         "correct": "ROM",
     }, {
-        "question": "What is the name of The Lion from The Chronicles of Narnia: The Lion, the Witch and the Wardrobe?",
+        "question": "Which of the following is not an output device? ",
         "choices": [
-            "Peter",
-            "Edmund",
-            "MR.Tumnus",
-            "Aslan"
+            "Speakers",
+            "Printer",
+            "Monitor",
+            "Keyboard"
         ],
-        "correct": "Aslan",
+        "correct": "Keyboard",
 
-    }];
+    },
+    {
+
+        "question": " Which of these is a not a computer manufacturer? ",
+        "choices": [
+            "Microsoft",
+            "Sun",
+            "IBM",
+            "Apple"
+        ],
+        "correct": "Microsoft",
+    },
+    {
+        "question": "Which of the following companies is famous for manufacturing computer processors? ",
+        "choices": [
+            "Corel",
+            "Sony",
+            "Intel",
+            "Macromedia"
+        ],
+        "correct": "Intel",
+
+    },
+    {
+        "question": "The capacity of your hard drive is measured in ",
+        "choices": [
+            "Mbps",
+            "MHZ",
+            "Gigabytes",
+            "52X"
+        ],
+        "correct": "Gigabytes",
+
+    },
+    {
+        "question": "Which device allows your computer to talk to other computers over a telephone line as well as access the internet?",
+        "choices": [
+            "CD-ROM drive",
+            "Modem",
+            "Hard Drive",
+            "RAM"
+        ],
+        "correct": "Modem",
+
+    },
+    {
+        "question": "Which part is the 'brain' of the computer?",
+        "choices": [
+            "ROM",
+            "CPU",
+            "Monitor",
+            "RAM"
+        ],
+        "correct": "CPU",
+
+    },
+    {
+        "question": "Who invented USB?",
+        "choices": [
+            "Intel",
+            "Microsoft",
+            "AMD",
+            "Samsung"
+        ],
+        "correct": "Intel",
+
+    },
+    {
+        "question": "Who invented the Graphical User Interface (GUI)?",
+        "choices": [
+            "Microsoft",
+            "Apple",
+            "Bell Labs",
+            "Xerox"
+        ],
+        "correct": "Xerox",
+
+    },
+    {
+        "question": "WWW stands for?",
+        "choices": [
+            "world word web",
+            "world wide web",
+            "world white web",
+            "world work web"
+        ],
+        "correct": "world wide web",
+
+    }
+    ];
     $("#start").on("click", function () {
 
         var triviadiv = $("<div id='triviaContainer'>");
         var timeRemaining = "<h2>Time Remaining:<span id='timeRemaining'></span></h2>";
-        var done = "<p><button id='done' type='submit'>DONE</button></p>";
+        var done = "<br><br><p><button id='done' type='submit'>DONE</button></p>";
         triviadiv.append(timeRemaining);
-        // var questions = "<ol><li><p>What is the permanent memory built into your computer called?</p>" +
-        //     "<input type='radio' name='q1Choices' id='opt1' value='CPU'>CPU" +
-        //     "<input type='radio' name='q1Choices' id='opt2' value='RAM'>RAM" +
-        //     "<input type='radio' name='q1Choices' id='opt3' value='ROM'>ROM" +
-        //     "<input type='radio' name='q1Choices' id='opt4' value='CD-ROM'>CD-ROM</li></ol>" +
-        //     "<input type='hidden' class='answer' value='ROM'>";
-
+        
         // iterate quiz array to display questions
         for (var i = 0; i < quiz.length; i++) {
             var question = quiz[i].question;
-            question = "<p>" + question + "</p>";
+            question = "<br><br><p>" + question + "</p>";
             triviadiv.append(question);
             var choicesLength = quiz[i].choices.length;
             // loop through choices for each question
@@ -88,6 +171,28 @@ $(document).ready(function () {
             displayResults();
         }
     }
+
+    // get checked value and correct value and compare both to display appropriate result
+    function evaluateResults() {
+        var selectedVal = "";
+        // iterate quiz array to evaluate correct answers
+        for (var i = 0; i < quiz.length; i++) {
+            var selected = $("input[type='radio'][name='q" + i + "Choices']:checked");
+            var correctAnswer = quiz[i].correct;
+            if (selected.length > 0) {
+                selectedVal = selected.val();
+                if (selectedVal === correctAnswer) {
+                    correct++;
+                }
+                else {
+                    inCorrect++;
+                }
+            }
+            else {
+                unanswered++;
+            }
+        }
+    }
     // function to display results once game is over
     function displayResults() {
         container.html("<div id='results'>");
@@ -95,28 +200,5 @@ $(document).ready(function () {
         container.append("<p> Correct Answers:" + correct + "</p>");
         container.append("<p> Incorrect Answers:" + inCorrect + "</p>");
         container.append("<p> Unanswered:" + unanswered + "</p>");
-    }
-    // get checked value and correct value and compare both to display appropriate result
-    function evaluateResults() {
-        var selectedVal = "";
-        // iterate quiz array to evaluate correct answers
-        for (var i = 0; i < quiz.length; i++) {
-                var selected = $("input[type='radio'][name='q" + i + "Choices']:checked");
-                var correctAnswer = quiz[i].correct;
-                if (selected.length > 0) {
-                    selectedVal = selected.val();
-                    if (selectedVal === correctAnswer) {
-                        correct++;
-                    }
-                    else {
-                        inCorrect++;
-                    }
-                }
-                else {
-                    unanswered++;
-                }
-            }
-
-        
     }
 });
